1cbf109331b11113991fb70d0d889c4c82f6f270
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-24 13:57:07 -0700

    added rollbar

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const card = document.querySelector(".worldCard")
 const btn = document.querySelector(".btn")
 
 const changeColor = () => {
-    let colors = ["maroon", "blue", "black", "purple", "darkcyan", "darkgreen", "darkred",]
+    let colors = ["maroon", "blue", "black", "purple", "darkcyan", "darkgreen", "darkred"]
     let randomNum = Math.floor(Math.random() * 7)
     let randomColor = colors[randomNum]
     card.style.backgroundColor = randomColor
--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const path = require('path')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '1fb3e376b6f440fb99f30672331f0ce7',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 })

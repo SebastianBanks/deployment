097f5cfc0fe809ff9816dda83ce41d1ab4132935
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-24 14:24:34 -0700

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const card = document.querySelector(".worldCard")
 const btn = document.querySelector(".btn")
 const form = document.querySelector("form")
 const input = document.querySelector("input")
+const axios = require('axios')
 
 const changeColor = () => {
     let colors = ["maroon", "blue", "black", "purple", "darkcyan", "darkgreen", "darkred"]

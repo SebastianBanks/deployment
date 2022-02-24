2c31542e31a0a733a8a00b88998b8873efaf0508
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-24 15:39:17 -0700

    yeah I know

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const btn = document.querySelector(".btn")
 const form = document.querySelector("form")
 const input = document.querySelector("input")
 
-let colors
+let colors = []
 const changeColor = () => {
     let colors = ["maroon", "blue", "black", "purple", "darkcyan", "darkgreen", "darkred"]
     let randomNum = Math.floor(Math.random() * 7)
@@ -59,7 +59,7 @@ const makeColor = (e) => {
 }
 
 getColors()
-wildShow()
+
 
 btn.addEventListener("click", changeColor)
 form.addEventListener("submit", makeColor)

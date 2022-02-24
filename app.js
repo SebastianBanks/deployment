c5b43996fab50973ffbb0b17114fae974b9eb6fc
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-24 16:30:13 -0700

    faster

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const timer = ms => new Promise(res => setTimeout(res, ms))
 async function load () { // We need to wrap the loop into an async function for this to work
   for (var i = 0; i < 3; i++) {
     console.log(i);
-    await timer(50); // then the created Promise can be awaited
+    await timer(5); // then the created Promise can be awaited
   }
 }
 
